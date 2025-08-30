e7c609b5bd5fc2acefaeba29bba7ada19353c795
Fernando <fernando@juandemariana.org>
2025-08-30 19:19:46 +0200

    Add Nuxt content to project.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@sidebase/nuxt-auth', '@nuxt/image', '@nuxt/eslint'],
+  modules: [
+    '@nuxt/icon',
+    '@sidebase/nuxt-auth',
+    '@nuxt/image',
+    '@nuxt/eslint',
+    '@nuxt/content',
+  ],
   app: {
     head: {
       title: 'Influencers directory',
