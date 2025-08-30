8f33526e44ae6b21a32e0fd0304abc50f38a0ec9
Fernando <fernando@juandemariana.org>
2025-08-30 19:18:13 +0200

    Add vueuse/head to project.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/eslint'],
+  app: {
+    head: {
+      title: 'Influencers directory',
+      meta: [{ name: 'description', content: 'Influencers directory' }],
+    },
+  },
 })
